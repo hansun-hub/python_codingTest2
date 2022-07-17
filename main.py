e1a07e43337649a6b1c55aac7ef09c3abb596559
hansun-hub <hansun33@naver.com>
2022-07-17 15:50:48 +0900

    two commit

--- main.py
+++ main.py
@@ -14,4 +14,17 @@
 #    print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("HELLO")
+a=int(input())
+if(a<0):
+    if(a%2==0):
+        print("A")
+    else:
+        print("B")
+else:
+    if(a%2==0):
+        print("C")
+    else:
+        print("D")
+        
+
+
